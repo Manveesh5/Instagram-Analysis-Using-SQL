332c5ebc91aec43557f1b544834c0b65e338f5e7
Manveesh5 <90413477+Manveesh5@users.noreply.github.com>
2023-10-17 05:45:02 +0530

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -2,4 +2,12 @@
 
 select * from users order by created_at asc limit 5; 
 
+/*Inactive User Engagement:Identify users who have never posted a single photo on Instagram.*/
+
+select users.id,username from users
+	 left join photos
+	 on users.id = photos.user_id where photos.id is NULL 
+	 order by username asc; 
+
+
 
