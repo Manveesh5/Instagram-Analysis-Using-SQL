e5095b2877d2b75029683bd5f4ccc7cc16badcea
Manveesh5 <90413477+Manveesh5@users.noreply.github.com>
2023-10-17 05:46:14 +0530

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -9,5 +9,11 @@ select users.id,username from users
 	 on users.id = photos.user_id where photos.id is NULL 
 	 order by username asc; 
 
+/*Contest Winner Declaration: The team has organized a contest where the user with the most likes on a single photo wins.*/
 
+select users.id ,users.username ,photos.image_url,count(*) as `Total Likes` from likes
+ inner join photos on photos.id = likes.photo_id
+ inner join users on users.id = likes.photo_id
+ group by photos.id
+ order by `Total Likes` desc limit 1; 
 
